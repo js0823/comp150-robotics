4df5a7ec2497e1dac5ccb2e8a783a0af4632cb2d
Jong Seo Yoon <js0823@gmail.com>
2017-11-07 15:36:15 -0500

    Create Pathify Package for start.

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jyoon10/gitrepo/comp150-robotics/catkin_ws/src
+# Build directory: /home/jyoon10/gitrepo/comp150-robotics/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(pathify)
---
+++ catkin_ws/build/pathify/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jyoon10/gitrepo/comp150-robotics/catkin_ws/src/pathify
+# Build directory: /home/jyoon10/gitrepo/comp150-robotics/catkin_ws/build/pathify
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
