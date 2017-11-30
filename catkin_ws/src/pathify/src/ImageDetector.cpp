c9fd603e2c855499590b03050975edcb2d2ff24b
Jong Seo Yoon <js0823@gmail.com>
2017-11-29 20:06:57 -0500

    Color detection. It will work with ranges, and will color the outerrim.

--- catkin_ws/src/pathify/src/ImageDetector.cpp
+++ catkin_ws/src/pathify/src/ImageDetector.cpp
@@ -7,6 +7,9 @@
 
 static const std::string OPENCV_WINDOW = "Turtlebot Live";
 
+// Detect color Red
+int Lower_H = 0, Lower_S = 100, Lower_V = 100;
+int Upper_H = 10, Upper_S = 255, Upper_V = 255;
 class ImageDetector
 {
     ros::NodeHandle nh_;
@@ -19,7 +22,7 @@ class ImageDetector
         : it_(nh_)
     {
         // Subscrive to input video feed and publish output video feed
-        image_sub_ = it_.subscribe("/camera/rgb/image_raw", 1, &ImageDetector::imageCb, this);
+        image_sub_ = it_.subscribe("/camera/rgb/image_raw", 1, &ImageDetector::ColorDetectionCallBack, this);
         image_pub_ = it_.advertise("/image_detector/output_video", 1);
 
         cv::namedWindow(OPENCV_WINDOW);
@@ -30,7 +33,7 @@ class ImageDetector
         cv::destroyWindow(OPENCV_WINDOW);
     }
 
-    void imageCb(const sensor_msgs::ImageConstPtr &msg)
+    void ColorDetectionCallBack(const sensor_msgs::ImageConstPtr &msg)
     {
         cv_bridge::CvImagePtr cv_ptr;
         try
@@ -43,17 +46,26 @@ class ImageDetector
             return;
         }
 
-        //cv::Mat img_mask, img_hsv;
-        //cv::cvtColor(cv_ptr->image, img_hsv, CV_BGR2HSV);
-        //cv::inRange(img_hsv,cv::Scalar(LowerH,LowerS,LowerV),cv::Scalar(UpperH,UpperS,UpperV),img_mask); 
+        cv::Mat img_mask, img_hsv;
+        cv::cvtColor(cv_ptr->image, img_hsv, CV_BGR2HSV);
+        cv::inRange(img_hsv,cv::Scalar(Lower_H, Lower_S, Lower_V),
+            cv::Scalar(Upper_H,Upper_S,Upper_V),img_mask);
 
-        // Draw an example circle on the video stream
-        // if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60) {
-        //     cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255, 0, 0));
-        // }
+        // find contour and draw line around it.
+        std::vector<std::vector<cv::Point> > contours;
+        std::vector<cv::Vec4i> hierarchy;
+
+        findContours(img_mask, contours, hierarchy, 
+            CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+        
+        for (int i = 0; i < contours.size(); i++) {
+            cv::drawContours(cv_ptr->image, contours, i, 
+                cv::Scalar(0,255,0), 2, 8, hierarchy, 0, cv::Point());
+        }
         
         // Update GUI Window
         cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+        //cv::imshow(OPENCV_WINDOW, img_mask);
         cv::waitKey(3);
 
         // Output modified video stream
