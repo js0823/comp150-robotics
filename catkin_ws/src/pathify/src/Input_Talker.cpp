472882f4e4277e8c0261152d62c57a99a58ecf69
Jong Seo Yoon <js0823@gmail.com>
2017-12-12 02:36:25 -0500

    Fixed InputTalker

--- catkin_ws/src/pathify/src/Input_Talker.cpp
+++ catkin_ws/src/pathify/src/Input_Talker.cpp
@@ -21,6 +21,12 @@ struct Coordinate
     float z;
 } currentCoordinate, homeCoordinate, targetCoordinate;
 
+void printCoordinate (Coordinate coord) {
+    std::cout << "x = " + std::to_string(coord.x) << std::endl;
+    std::cout << "y = " + std::to_string(coord.y) << std::endl;
+    std::cout << "z = " + std::to_string(coord.z) << std::endl;
+}
+
 struct Item
 {
     std::string maze_name;
@@ -40,7 +46,6 @@ std::string maze_name;
 ros::ServiceClient client;
 ros::Subscriber sub;
 ros::Subscriber position_sub;
-ros::Publisher chatter_pub;
 
 bool found_before(std::string item_name);
 bool is_empty(std::ifstream &pFile);
@@ -72,7 +77,6 @@ int main(int argc, char **argv)
 
     ros::init(argc, argv, "Input_Talker");
     ros::NodeHandle n;
-    chatter_pub = n.advertise<std_msgs::String>("user_responds", 1000);
     position_sub = n.subscribe<geometry_msgs::Pose>("/robot_pose", 1000, positionCallback);
     //client = n.serviceClient<pathify::ReturnCoordinate>("coordinate");
     std::system("rosservice call /StartMapping");
@@ -83,6 +87,8 @@ int main(int argc, char **argv)
     homeCoordinate.x = currentCoordinate.x;
     homeCoordinate.y = currentCoordinate.y;
     homeCoordinate.z = currentCoordinate.z;
+    std::cout << "Your homeCoordinate is " << std::endl;
+    printCoordinate(homeCoordinate);
 
     // Get Name of the map
     bool got_MapName = false;
@@ -108,16 +114,11 @@ int main(int argc, char **argv)
     {
         std::string input;
 
-        std::cout << "What do you want me to do?"
-                  << "\n";
-        std::cout << "Commands are [go, find, quit]"
-                  << "\n";
-        std::cout << "Ex. Go to red ----> Will go to red if the red location is saved in the database."
-                  << "\n";
-        std::cout << "Ex. Find red ----> Will start exploring the map."
-                  << "\n";
-        std::cout << "Ex. quit, end, stop, terminate ----> Exit this program."
-                  << "\n";
+        std::cout << "What do you want me to do?" << "\n";
+        std::cout << "Commands are [go, find, quit]" << "\n";
+        std::cout << "Ex. Go to red ----> Will go to red if the red location is saved in the database." << "\n";
+        std::cout << "Ex. Find red ----> Will start exploring the map." << "\n";
+        std::cout << "Ex. quit, end, stop, terminate ----> Exit this program." << "\n";
         getline(std::cin, input);
         std::vector<std::string> input_vector;
         input_vector = parse_input(input);
@@ -241,6 +242,7 @@ void go_mode(std::string item_name)
     {
         if (Robot_RAM[i].item_name == item_name && Robot_RAM[i].maze_name == maze_name)
         {
+            std::cout << "Going to " << " " << item_name + "..." << "\n";
             Coordinate x_y_z;
             x_y_z.x = Robot_RAM[i].location.x;
             x_y_z.y = Robot_RAM[i].location.y;
@@ -253,27 +255,22 @@ void go_mode(std::string item_name)
             nav2d_navigator::MoveToPosition2DGoal goal;
 	        goal.target_pose.x = x_y_z.x;
 	        goal.target_pose.y = x_y_z.y;
-	        //goal.target_pose.theta = tf::getYaw(msg->pose.orientation);
-	        //goal.target_distance = 0.25;
-	        //goal.target_angle = 0.1;
+	
+            ac.sendGoal(goal);
+            ac.waitForResult();
+            std::cout << "Found " << item_name << ". Now going back to home location." << std::endl;
+	        goal.target_pose.x = homeCoordinate.x;
+            goal.target_pose.y = homeCoordinate.y;
 	
             ac.sendGoal(goal);
             ac.waitForResult();
 
-            /* TODO: send xyz timi*/
-            std::cout << "Going to "
-                      << " " << item_name + "..."
-                      << "\n";
-            std::cout << "Goon to "
-                      << " " << item_name + "!"
-                      << "\n";
             return;
         }
     }
 }
 
-void find_mode(std::string item_name, ros::NodeHandle n)
-{
+void find_mode(std::string item_name, ros::NodeHandle n) {
     /*look for and add to memory*/
     Item tmp;
     bool found = false;
@@ -282,11 +279,9 @@ void find_mode(std::string item_name, ros::NodeHandle n)
     //pathify::ReturnCoordinate srv;
     std::system("rosservice call /StartExploration");
 
-    while (!found)
-    {
+    while (!found) {
         sub = n.subscribe("imageDetector_output", 1000, chatterCallback);
-        if (message_from_openCV == "Stop")
-        {
+        if (message_from_openCV == "Stop") {
             std::system("rosservice call /Stop");
             std::cout << " Is this the location? (y/n)"
                       << "\n";
@@ -294,53 +289,43 @@ void find_mode(std::string item_name, ros::NodeHandle n)
             getline(std::cin, responds);
             std::vector<std::string> in;
             in = parse_input(responds);
-            if (change_to_lowercase(in[0]) == "y")
-            {
-                /*TODO: get co-ordinated from timi*/
+            if (change_to_lowercase(in[0]) == "y") {
+                // Save the location to the database
+                Coordinate x_y_z;
+                x_y_z.x = currentCoordinate.x;
+                x_y_z.y = currentCoordinate.y;
+                x_y_z.z = currentCoordinate.z;
+                tmp.location = x_y_z;
+                Robot_RAM.push_back(tmp);
+                std::string tmp_string = parse_Item_To_string(tmp);
+                update_rom("Robot_ROM.txt", tmp_string);
+
+                // Move back to home location
+                std::cout << "Moving back to the home location." << std::endl;
+                MoveClient ac("MoveTo", true);
+                while(!ac.waitForServer(ros::Duration(5.0))) {
+                    ROS_INFO("Waiting for the MoveTo action server to come up");
+                }
+                nav2d_navigator::MoveToPosition2DGoal goal;
+	            goal.target_pose.x = homeCoordinate.x;
+                goal.target_pose.y = homeCoordinate.y;
+                ac.sendGoal(goal);
+                ac.waitForResult();
+
                 found = true;
             }
-            else if (change_to_lowercase(in[0]) == "n")
-            {
-                for (int i = 0; i < 4; i++)
-                {
-                    std_msgs::String msg;
-                    std::stringstream ss;
-                    ss << "keep_going";
-                    msg.data = ss.str();
-                    chatter_pub.publish(msg);
-                }
+            else if (change_to_lowercase(in[0]) == "n") {
+                std::cout << "Ok Let's keep exploring." << std::endl;
+                std::system("rosservice call /StartExploration");
             }
         }
     }
-
-    /* if (client.call(srv))
-     *    {
-     *        /*recieve co-ordinate from robot*
-     *        ROS_INFO("result: %s", srv.response.coord_xyz);
-     *        std::string result = srv.response.coord_xyz;
-     *        std::vector<std::string> tmp_result = parse_output_from_ros(result);
-     *        Coordinate x_y_z;
-     *        float size  = string_to_float(tmp_result[0]) + string_to_float(tmp_result[1]) + string_to_float(tmp_result[2]);
-     *        if(size!= 0.0)
-     *        {
-     *            x_y_z.x =  string_to_float(tmp_result[0]);                           
-     *            x_y_z.y =  string_to_float(tmp_result[1]);                          
-     *            x_y_z.z =  string_to_float(tmp_result[2]);                          
-     *            tmp.location = x_y_z;
-     *            Robot_RAM.push_back(tmp);
-     *            std::string tmp_string = parse_Item_To_string(tmp);
-     *            update_rom("Robot_ROM.txt", tmp_string);
-     *            std::cout<<"Found"<<" "<< item_name<<"\n";
-}
-}*/
 }
 
-void update_rom(const std::string &file_name, const std::string &content)
-{
+void update_rom(const std::string &file_name, const std::string &content) {
     std::ofstream outfile;
     outfile.open(file_name.c_str(), std::ios_base::app);
-    if (outfile.is_open())
-    {
+    if (outfile.is_open()) {
         outfile << content;
         outfile.close();
         return;
@@ -349,17 +334,13 @@ void update_rom(const std::string &file_name, const std::string &content)
               << "\n";
 }
 
-/*reads from file/database and updat RAM Memory*/
-void update_ram(const std::string &file_name)
-{
+/*reads from file/database and update RAM Memory*/
+void update_ram(const std::string &file_name) {
     std::string line;
     std::ifstream myfile(file_name.c_str());
-    if (myfile.is_open())
-    {
-        if (!is_empty(myfile))
-        {
-            while (getline(myfile, line))
-            {
+    if (myfile.is_open()) {
+        if (!is_empty(myfile)) {
+            while (getline(myfile, line)) {
                 Item tmp = parse_string_To_Item(line);
                 Robot_RAM.push_back(tmp);
             }
@@ -368,21 +349,18 @@ void update_ram(const std::string &file_name)
     }
 }
 
-bool is_empty(std::ifstream &pFile)
-{
+bool is_empty(std::ifstream &pFile) {
     return pFile.peek() == std::ifstream::traits_type::eof();
 }
 
-std::string parse_Item_To_string(Item item)
-{
+std::string parse_Item_To_string(Item item) {
     std::string fin;
     fin = item.maze_name + ";" + item.item_name + ";" + float_to_string(item.location.x) + ";" +
           float_to_string(item.location.y) + "." + "\n";
     return fin;
 }
 
-std::vector<std::string> parse_helper(std::string str)
-{
+std::vector<std::string> parse_helper(std::string str) {
     int size = str.length();
     std::string a;
     std::vector<std::string> output;
@@ -402,20 +380,16 @@ std::vector<std::string> parse_helper(std::string str)
     return output;
 }
 
-std::vector<std::string> parse_output_from_ros(std::string str)
-{
+std::vector<std::string> parse_output_from_ros(std::string str) {
     int size = str.length();
     std::string a;
     std::vector<std::string> output;
-    for (int i = 0; i < size; i++)
-    {
-        if (str[i] == ',' || str[i] == '.')
-        {
+    for (int i = 0; i < size; i++) {
+        if (str[i] == ',' || str[i] == '.') {
             output.push_back(a);
             a = "";
         }
-        else
-        {
+        else {
             a += str[i];
         }
     }
@@ -423,8 +397,7 @@ std::vector<std::string> parse_output_from_ros(std::string str)
     return output;
 }
 
-Item parse_string_To_Item(std::string item_stringform)
-{
+Item parse_string_To_Item(std::string item_stringform) {
     Item tmp;
     std::vector<std::string> items;
     Coordinate x_y_z;
@@ -442,66 +415,53 @@ Item parse_string_To_Item(std::string item_stringform)
     return tmp;
 }
 
-bool found_before(std::string item_name)
-{
+bool found_before(std::string item_name) {
     int size = (int)Robot_RAM.size();
-    for (int i = 0; i < size; i++)
-    {
-        if (Robot_RAM[i].item_name == item_name && Robot_RAM[i].maze_name == maze_name)
-        {
+    for (int i = 0; i < size; i++) {
+        if (Robot_RAM[i].item_name == item_name && Robot_RAM[i].maze_name == maze_name) {
             return true;
         }
     }
     return false;
 }
 
-std::string change_to_lowercase(std::string str)
-{
+std::string change_to_lowercase(std::string str) {
     int size = str.length();
     std::string a;
-    for (int i = 0; i < size; i++)
-    {
+    for (int i = 0; i < size; i++) {
         a += tolower(str[i]);
     }
     return a;
 }
 
-float string_to_float(std::string str)
-{
+float string_to_float(std::string str) {
     std::string::size_type sz; // alias of size_t
     float flt = std::stof(str, &sz);
     return flt;
 }
 
-std::string float_to_string(float flt)
-{
+std::string float_to_string(float flt) {
     return std::to_string(flt);
 }
 
-std::vector<std::string> parse_input(std::string str)
-{
+std::vector<std::string> parse_input(std::string str) {
     int size = str.length();
     std::string a;
     std::vector<std::string> output;
-    for (int i = 0; i < size; i++)
-    {
-        if (!isspace(str[i]))
-        {
+    for (int i = 0; i < size; i++) {
+        if (!isspace(str[i])) {
             a += str[i];
         }
 
-        else
-        {
-            if (a != "")
-            {
+        else {
+            if (a != "") {
                 output.push_back(a);
                 a = "";
             }
         }
     }
 
-    if (a != "")
-    {
+    if (a != "") {
         output.push_back(a);
         a = "";
     }
